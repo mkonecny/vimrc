e2d4d9b6b4abf64486f65704c78b921ff00c0b42
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-05-08 00:12:41 -0400

    added ability to save last location with redshift

--- redshift.py
+++ redshift.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-
+from os.path import expanduser, exists
+import sys
 
 # TODO : turn this script into a full launcher of all of mine background utils
 # set the default backlight as well (35)
-
 
 import json
 import urllib2
@@ -11,18 +11,37 @@ from subprocess import Popen
 
 url = 'http://freegeoip.net/json/'
 
-response = urllib2.urlopen(url)
+settings_file = expanduser("~/.redshift_location")
 
-data = json.loads(response.read())
+data = None
+
+try:
+    response = urllib2.urlopen(url)
+    data = json.loads(response.read())
+    write_settings(settings_file, data)
+except:
+    data = read_settings(expanduser(settings_file))
 
 # lat:lon
 args = "-l %s:%s" % (data[u'latitude'], data[u'longitude'])
-cmd = "gtk-redshift" # "redshift" for without the gui
+cmd = "gtk-redshift"  # "redshift" for without the gui
 full_cmd = "%s %s" % (cmd, args)
 
 print("Running with '%s'" % full_cmd)
 
-Popen(["xbacklight","-set","35"])
+Popen(["xbacklight", "-set", "35"])
 Popen([cmd, args])
 
 
+def write_settings(path, settings):
+    with open(path, mode='w') as f:
+        f.write(json.dumps(settings))
+    print("Wrote settings to %s" % path)
+
+
+def read_settings(path):
+    if not exists(path):
+        print("Cannot read settings from %s" % path)
+        sys.exit(1)
+    with open(path) as f:
+        return(json.loads(f.read()))
