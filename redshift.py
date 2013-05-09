4bb9db8f06604d9a9d98841cb369499cbaa9a74a
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-05-09 08:39:20 -0400

    fixed use before definition

--- redshift.py
+++ redshift.py
@@ -9,6 +9,20 @@ import json
 import urllib2
 from subprocess import Popen
 
+
+def write_settings(path, settings):
+    with open(path, mode='w') as f:
+        f.write(json.dumps(settings))
+    print("Wrote settings to %s" % path)
+
+
+def read_settings(path):
+    if not exists(path):
+        print("Cannot read settings from %s" % path)
+        sys.exit(1)
+    with open(path) as f:
+        return(json.loads(f.read()))
+
 url = 'http://freegeoip.net/json/'
 
 settings_file = expanduser("~/.redshift_location")
@@ -31,17 +45,3 @@ print("Running with '%s'" % full_cmd)
 
 Popen(["xbacklight", "-set", "35"])
 Popen([cmd, args])
-
-
-def write_settings(path, settings):
-    with open(path, mode='w') as f:
-        f.write(json.dumps(settings))
-    print("Wrote settings to %s" % path)
-
-
-def read_settings(path):
-    if not exists(path):
-        print("Cannot read settings from %s" % path)
-        sys.exit(1)
-    with open(path) as f:
-        return(json.loads(f.read()))
